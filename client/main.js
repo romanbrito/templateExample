7cbeea9bc45600d2939ad3cbb55082450e316089
Roman Brito <romanbrito1@gmail.com>
2017-06-28 16:10:38 -0500

    some scroll fire

--- client/main.js
+++ client/main.js
@@ -30,6 +30,24 @@ Template.Nav_bar.onRendered(function () {
 
   });
 
+Template.Scroll_fire.onRendered(function () {
+  var options = [
+    {selector: '#staggered-test', offset: 50, callback: function(el) {
+      Materialize.toast("This is our ScrollFire Demo!", 1500 );
+    } },
+    {selector: '#staggered-test', offset: 205, callback: function(el) {
+      Materialize.toast("Please continue scrolling!", 1500 );
+    } },
+    {selector: '#staggered-test', offset: 400, callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    } },
+    {selector: '#image-test', offset: 500, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } }
+  ];
+  Materialize.scrollFire(options);
+});
+
 // Meteor.startup(function () {
 //   $(window).resize(function (evt) {
 //
